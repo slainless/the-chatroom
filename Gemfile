dc96a9ed95bde66dc7a2fa5cccd7183fe044d7cb
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2023-04-05 10:40:41 +0800

    (backend): update seeder & added faker

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", "~> 5.0"
 
 gem "jwt", "~> 2.7"
 
+gem "faker"
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "faker"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,11 +19,15 @@
 #
 #
 
+user_ids = (1..10).map do
+  Time.at(rand * Time.now.to_i).to_i * 1000
+end
+
 (1..100).each do |i|
   PublicMessage.create(
-    user_id: rand(0..9),
+    user_id: user_ids.sample,
     room_id: Rails.application.config.allowed_public_rooms.sample,
-    body: "body_#{i}",
+    body: Faker::Lorem.__send__(%w[sentence word paragraph question].sample),
     created_at: DateTime.now - (100 - i + Random.rand).hour
   )
 end
